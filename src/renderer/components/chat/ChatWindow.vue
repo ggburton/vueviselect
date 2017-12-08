85958021030d3a4a066cd0c5bd7bd33be6b6faa6
Greg Burton <GBurton@wcc.qld.edu.au>
2017-12-08 14:21:16 +1000

    textarea is now at bottom of message box and inside parent div

--- src/renderer/components/chat/ChatWindow.vue
+++ src/renderer/components/chat/ChatWindow.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="chatWindowContainer">
-    <chat-message v-for="message in $store.getters.getMessages" :key="message.text" :message="message"></chat-message>
+    <div class="messages">
+      <chat-message v-for="message in $store.getters.getMessages" :key="message.text" :message="message"></chat-message>
+    </div>
     <div class="textEntryDiv">
       <textarea autofocus class="testarea" rows="5" v-model="draftMessage"></textarea>
       <button @click="send">Send</button>
@@ -39,18 +41,27 @@ export default {
 
 <style>
 .chatWindowContainer {
+  display: grid;
+  grid-template-rows: repeat(10, 10%);
+  grid-template-columns: repeat(8, 12.5%);
   height: 100vh;
 }
 
+.messages {
+  grid-row: 1 / span 9;
+  grid-column: 1 / span 8;
+}
+
 textarea {
-  width: 99%;
+  min-width: 90%;
   outline: none;
   resize: none;
 }
 
 .textEntryDiv {
+  grid-row: 10 / span 1;
+  grid-column: 1 / span 8;
   margin-bottom: 5px;
-  align-self: bottom;
+  display: flex;
 }
-
 </style>
