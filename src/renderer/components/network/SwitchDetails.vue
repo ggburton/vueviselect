98f233d17097abf85ff144f7f966674929fcd00e
Greg Burton <GBurton@wcc.qld.edu.au>
2017-12-11 16:19:00 +1000

    style change on switch detail table

--- src/renderer/components/network/SwitchDetails.vue
+++ src/renderer/components/network/SwitchDetails.vue
@@ -5,20 +5,30 @@
       <h1>{{ wSwitch.name }}</h1>
     </div>
 
-    <div class="label">IP Address</div>
-    <div class="data">{{ wSwitch.ipAddress }}</div>
+    <div class="data-row">
+      <div class="label">IP Address</div>
+      <div class="data">{{ wSwitch.ipAddress }}</div>
+    </div>
 
-    <div class="label">Parent Switch</div>
-    <div class="data">{{ wSwitch.parent }}</div>
+    <div class="data-row">
+      <div class="label">Parent Switch</div>
+      <div class="data">{{ wSwitch.parent }}</div>
+    </div>
 
-    <div class="label">Last Ping</div>
-    <div class="data">{{ lastPingTime }}</div>
+    <div class="data-row">
+      <div class="label">Last Ping</div>
+      <div class="data">{{ lastPingTime }}</div>
+    </div>
 
-    <div class="label">Location</div>
-    <div class="data">{{ wSwitch.location }}</div>
+    <div class="data-row">
+      <div class="label">Location</div>
+      <div class="data">{{ wSwitch.location }}</div>
+    </div>
 
-    <div class="label">Uptime</div>
-    <div class="data">{{ switchUptime }}</div>
+    <div class="data-row">
+      <div class="label">Uptime</div>
+      <div class="data">{{ switchUptime }}</div>
+    </div>
 
   </div>
 </template>
@@ -61,39 +71,32 @@ export default {
 
 <style>
 .data-grid {
-  display: grid;
-  grid-template-rows: repeat(6, auto);
-  grid-template-columns: repeat(2, 1fr);
-  grid-auto-flow: column;
-  grid-row-gap: 5px;
+  display: flex;
+  flex-direction: column;
   border: 3px solid black;
   border-radius: 8px;
   margin-left: 8px;
 }
 
 .heading {
-  grid-column: 1 / -1;
-  grid-row: 1/ span 1;
   width: 100%;
   background-color: #d8d8d8;
   text-align: center;
 }
 
+.data-row {
+  display: flex;
+  justify-content: space-between;
+}
+
 .label {
-  grid-column: 1 / span 1;
-  font-size: 1.3em;
-  font-weight: bold;
+  width: 50%;
   margin-left: 8px;
 }
 
 .data {
-  grid-column: 2 / span 1;
-  font-size: 1.2em;
-  font-weight: bold;
+  width: 50%;
 }
 
-.address {
-  grid-row: 2 /span 1;
-}
 
 </style>
